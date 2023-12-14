0aa55d21b07b424f79c5f4acdd0fac09cebe9147
Justice Ampofo <justiceampofo@gmail.com>
2023-12-13 22:56:24 -0500

    bug fixes and improvements

--- root/js/game_logic.js
+++ root/js/game_logic.js
@@ -1,15 +1,11 @@
-if (window.orientation == undefined) {
-	alert('This game is meant for smartphones and tablets');
-}
-
-//Get reference to the body element
+// Get reference to the body element
 var body = document.getElementById('myBody');
 
-//Get reference to the div elements
+// Get reference to the div elements
 var div = document.getElementById('myDiv');
 var div2 = document.getElementById('mySecondDiv');
 
-//Get reference to restart and play buttons
+// Get reference to restart and play buttons
 var restart = document.getElementById('myRestartButton');
 var engage = document.getElementById('myPlayButton');
 
@@ -21,7 +17,7 @@ var context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-//This for the high score text in the game over function
+// This is for the high score text in the game over function
 var h = canvas.height / 2 + 100 + 'px';
 
 //player
@@ -29,11 +25,11 @@ var player = {
 	// Keep track of player's position
 	posX: canvas.width / 2,
 	posY: canvas.height / 2,
-	rad: 50, //player's radius
+	rad: 50, // Player's radius
 	faceCol: 'blue',
 };
 
-//goal
+// Goal
 var goal = {
 	posX: Math.round(Math.random() * canvas.width),
 	posY: Math.round(Math.random() * canvas.height),
@@ -41,7 +37,7 @@ var goal = {
 	lineThick: 5,
 };
 
-//Color array
+// Color array
 var myColorArray = ['grey', 'red', 'orange', 'brown', 'purple'];
 
 var colRand = 0;
@@ -51,7 +47,7 @@ function setRandom() {
 	goal.posY = Math.round(Math.random() * canvas.height);
 }
 
-//This function determines which color to pick next
+// This function determines which color to pick next
 function setColor() {
 	colRand++;
 	if (colRand == myColorArray.length) {
@@ -60,7 +56,7 @@ function setColor() {
 	body.style.background = myColorArray[colRand];
 }
 
-//draws the player but bigger in the game over function
+// Draws the player but bigger in the game over function
 function drawStationPlayer(x, y) {
 	context.beginPath();
 	context.fillStyle = 'blue';
@@ -68,26 +64,35 @@ function drawStationPlayer(x, y) {
 	context.arc(x, y, 80, 0, Math.PI * 2);
 	context.fill();
 
-	//Draw player's left eye
+	// Draw player's left eye
 	context.beginPath();
 	context.arc(x - 25, y - 30, 20, 0, Math.PI * 2);
 	context.fillStyle = 'white';
 	context.fill();
 
-	//Draw player's right eye
+	// Draw player's right eye
 	context.beginPath();
 	context.arc(x + 25, y - 30, 20, 0, Math.PI * 2);
 	context.fillStyle = 'white';
 	context.fill();
 
-	//Draw player's mouth
+	// Draw player's mouth
 	context.beginPath();
 	context.arc(x, y + 15, 40, 0, Math.PI);
 	context.fillStyle = 'white';
 	context.fill();
 }
 
-//game variables
+// Write high score
+function writeHighScore() {
+	context.fillText(
+		'High Score: ' + highScore,
+		canvas.width / 2,
+		canvas.height / 2 + 50
+	);
+}
+
+// Game variables
 var score = 0;
 var counter;
 var timer;
@@ -114,7 +119,7 @@ if (typeof Storage != 'undefined') {
 	alert("Your browswer won't save the High Score :( ");
 }
 
-//The game begins
+// The game begins
 function startGame() {
 	counter = 60;
 	score = 0;
@@ -123,8 +128,8 @@ function startGame() {
 	// Listen for device orientation event
 	window.ondeviceorientation = function (eventData) {
 		// Move the player
-		player.posX = player.posX + event.gamma;
-		player.posY = player.posY + event.beta;
+		player.posX = player.posX + eventData.gamma;
+		player.posY = player.posY + eventData.beta;
 
 		//the Distance between player and goal
 		var dx = goal.posX - player.posX;
@@ -173,81 +178,58 @@ function startGame() {
 		context.fillStyle = 'white';
 		context.fill();
 
-		//Draw player's right eye
+		// Draw player's right eye
 		context.beginPath();
 		context.arc(player.posX + 15, player.posY - 15, 10, 0, Math.PI * 2);
 		context.fillStyle = 'white';
 		context.fill();
 
-		//Draw player's mouth
+		// Draw player's mouth
 		context.beginPath();
 		context.arc(player.posX, player.posY + 10, 20, 0, Math.PI);
 		context.fillStyle = 'white';
 		context.fill();
 
-		//font
+		// Font
 		context.font = '40px Verdana';
 		// Horizontal text alignment
 		context.textAlign = 'center';
 
 		// Vertical text alignment, default is "alphabetic"
 		context.textBaseline = 'middle';
-		//writing text
+		// Writing text
 		context.fillStyle = 'white';
 		context.fillText('Score: ' + score, 100, 25);
 		context.fillText('Time: ' + counter, 300, 25);
 		context.fillText('High Score: ' + highScore, 650, 25);
-	}; //ondeviceorientation ends here
-} //startGame ends
+	}; // ondeviceorientation ends here
+} // StartGame ends
 
-//Starts when the app lanches
+// Starts when the app lanches
 function MainMenu() {
-	window.ondeviceorientation = null;
-	clearInterval(timer);
-	context.clearRect(0, 0, canvas.width, canvas.height);
-
-	context.beginPath();
-	context.textBaseline = 'middle';
-	context.textAlign = 'center';
-	context.font = '40px Verdana';
-	context.fillStyle = 'white';
+	reset();
 	context.fillText(
 		'Rotate your device to collect the coins!!',
 		canvas.width / 2,
 		canvas.height / 2
 	);
-	context.fillText(
-		'High Score: ' + highScore,
-		canvas.width / 2,
-		canvas.height / 2 + 50
-	);
+	writeHighScore();
 	context.fill();
 
-	drawStationPlayer(canvas.width / 2, canvas.height / 2 - 100);
+	drawStationPlayer(canvas.width / 2, canvas.height / 2 - 125);
 	div2.style.display = 'block';
 	div2.style.top = h;
-} //MainMenu function ends here
+} // MainMenu function ends here
 
-//When time runs out
+// When time runs out
 function gameOver() {
-	window.ondeviceorientation = null;
-	clearInterval(timer);
-	context.clearRect(0, 0, canvas.width, canvas.height);
+	reset();
 	alert('Time is up!!!');
-	context.beginPath();
-	context.textBaseline = 'middle';
-	context.textAlign = 'center';
-	context.font = '40px Verdana';
-	context.fillStyle = 'white';
 	context.fillText('Score: ' + score, canvas.width / 2, canvas.height / 2);
-	context.fillText(
-		'High Score: ' + highScore,
-		canvas.width / 2,
-		canvas.height / 2 + 50
-	);
+	writeHighScore();
 	context.fill();
 
-	drawStationPlayer(canvas.width / 2, canvas.height / 2 - 100);
+	drawStationPlayer(canvas.width / 2, canvas.height / 2 - 125);
 	div.style.display = 'block';
 	div.style.top = h;
 
@@ -262,21 +244,93 @@ function gameOver() {
 			canvas.height / 2 - 250
 		);
 	}
-} //GameOver function ends here
+} // GameOver function ends here
 
-//restart button pressed
+// When the user is not on a mobile or tablet
+function notOnMobileOrTablet() {
+	reset();
+	context.fillText(
+		'Please use this application on a phone or tablet.',
+		canvas.width / 2,
+		canvas.height / 2
+	);
+	context.fill();
+
+	drawStationPlayer(canvas.width / 2, canvas.height / 2 - 150);
+}
+
+// If the user rejects the request for access to motion and orientation or if another error occurs
+function requestRejected(message) {
+	reset();
+	context.fillText(message, canvas.width / 2, canvas.height / 2);
+}
+
+function reset() {
+	window.ondeviceorientation = null;
+	clearInterval(timer);
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.beginPath();
+	context.textBaseline = 'middle';
+	context.textAlign = 'center';
+	context.font = '40px Verdana';
+	context.fillStyle = 'white';
+}
+
+function mobileAndTabletCheck() {
+	let check = false;
+	(function (a) {
+		if (
+			/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+				a
+			) ||
+			/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+				a.substr(0, 4)
+			)
+		)
+			check = true;
+	})(navigator.userAgent || navigator.vendor || window.opera);
+	return check;
+}
+
+function motionRequestion() {
+	if (typeof DeviceMotionEvent.requestPermission === 'function') {
+		// Handle iOS 13+ devices.
+		DeviceMotionEvent.requestPermission()
+			.then((state) => {
+				if (state === 'granted') {
+					startGame();
+					countDown();
+				} else {
+					console.error('Request to access the orientation was rejected');
+					requestRejected('Request to access the orientation was rejected');
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+				requestRejected('Sorry… Something went wrong');
+			});
+	} else {
+		// Handle regular non iOS 13+ devices.
+		startGame();
+		countDown();
+	}
+}
+
+// Restart button pressed
 restart.onclick = function () {
 	div.style.display = 'none';
 	startGame();
-	//countdown timer
 	countDown();
 };
 
-//start game button pressed
+// Start game button pressed
 engage.onclick = function () {
 	div2.style.display = 'none';
-	startGame();
-	countDown();
+	motionRequestion();
 };
 
-MainMenu();
+if (!mobileAndTabletCheck()) {
+	notOnMobileOrTablet();
+} else {
+	MainMenu();
+}
